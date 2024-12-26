5d88a466b7349a6d2e23dc8e46bb0854bb053986
Aleksandar <milicalex@gmail.com>
2024-12-26 14:17:23 +0100

    custom generator

--- pkg/swagger/tmf641v4_1/gen.go
+++ pkg/swagger/tmf641v4_1/gen.go
@@ -1,5 +1,5 @@
 package tmf641v4_1
 
 //go:generate rm -rf server
-//go:generate mkdir -p server -t ./template
-//go:generate swagger generate server --quiet --target server --name service-ordering-v4_1 --spec TMF641-ServiceOrdering-v4.1.0.yaml --exclude-main
+//go:generate mkdir -p server
+//go:generate swagger generate server -t ./template --quiet --target server --name service-ordering-v4_1 --spec TMF641-ServiceOrdering-v4.1.0.yaml --exclude-main
