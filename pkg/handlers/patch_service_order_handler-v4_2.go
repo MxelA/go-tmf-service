d9ba589236034096e7eb140aa3eebe7ffacc2779
Aleksandar <milicalex@gmail.com>
2025-03-01 21:44:25 +0100

    add supprot for merge patch service order

---
+++ pkg/handlers/patch_service_order_handler-v4_2.go
@@ -0,0 +1,82 @@
+package handler_v4_2
+
+import (
+	database "github.com/MxelA/tmf-service-go/pkg/config"
+	"github.com/MxelA/tmf-service-go/pkg/swagger/tmf641v4_2/server/models"
+	"github.com/MxelA/tmf-service-go/pkg/swagger/tmf641v4_2/server/restapi/operations/service_order"
+	"github.com/go-openapi/runtime/middleware"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"log"
+	"strings"
+)
+
+func PatchServiceOrderHandler(req service_order.PatchServiceOrderParams) middleware.Responder {
+	contentType := req.HTTPRequest.Header["Content-Type"][0]
+
+	if strings.Contains(contentType, "application/json-patch+json") {
+		return processJsonPatch(req)
+	} else if strings.Contains(contentType, "application/merge-patch+json") {
+		return processMergePatch(req)
+	}
+
+	errCode := "422"
+	reason := "Unsupported Media Type"
+	errModel := models.Error{
+		Reason:  &reason,
+		Code:    &errCode,
+		Message: "Unsupported media type" + contentType,
+	}
+
+	return service_order.NewPatchServiceOrderInternalServerError().WithPayload(&errModel)
+}
+
+func processMergePatch(req service_order.PatchServiceOrderParams) middleware.Responder {
+	errCode := "422"
+	reason := "Unsupported media type "
+	var errModel = models.Error{
+		Reason:  &reason,
+		Code:    &errCode,
+		Message: "Unsupported 'application/json-patch+json' media type ",
+	}
+	return service_order.NewPatchServiceOrderInternalServerError().WithPayload(&errModel)
+}
+
+func processJsonPatch(req service_order.PatchServiceOrderParams) middleware.Responder {
+	serviceOrderId, err := primitive.ObjectIDFromHex(req.ID)
+
+	if err != nil {
+		errCode := "500"
+		reason := err.Error()
+		var errModel = models.Error{
+			Reason:  &reason,
+			Code:    &errCode,
+			Message: "Internal server error",
+		}
+		return service_order.NewPatchServiceOrderInternalServerError().WithPayload(&errModel)
+	}
+
+	// Get mongoDB instance
+	mg := database.GetMongoInstance()
+	collection := mg.Db.Collection("serviceOrder")
+
+	filter := bson.M{"_id": serviceOrderId}
+	update := bson.M{"$set": req.ServiceOrder}
+	record := collection.FindOneAndUpdate(req.HTTPRequest.Context(), filter, update)
+
+	updateServiceOrder := models.ServiceOrder{}
+	err = record.Decode(&updateServiceOrder)
+	if err != nil {
+		errCode := "500"
+		reason := err.Error()
+		var errModel = models.Error{
+			Reason:  &reason,
+			Code:    &errCode,
+			Message: "Internal server error",
+		}
+		log.Println(err)
+		return service_order.NewPatchServiceOrderInternalServerError().WithPayload(&errModel)
+	}
+
+	return service_order.NewPatchServiceOrderOK().WithPayload(&updateServiceOrder)
+}
